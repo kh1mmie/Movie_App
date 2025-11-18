d42663a17a0e85354af85f03645e9be4b9eff490
kh1mmie <khimmie40@gmail.com>
2025-11-18 16:02:01 +0700

    Clear API keys in config.js
    Removed sensitive API keys from config.js.

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const config = () => {
     return {
-        TMDB_API_KEY: 'fb71ae335c16ce421a44d897306132a5',
-        FIREBASE_API_KEY: 'AIzaSyB3g-fOO2pWnjJSv7b0NAeNUoObxV_HSG4',
-        FIREBASE_AUTH_DOMAIN: 'movie-app-52d95.firebaseapp.com',
-        FIREBASE_PROJECT_ID: 'movie-app-52d95',
-        FIREBASE_STORAGE_BUCKET: 'movie-app-52d95.appspot.com',
-        FIREBASE_MESSAGING_SENDER_ID: '948271946553',
-        FIREBASE_APP_ID: '1:948271946553:web:f4ddb0f43899622b84e6e8'
+        TMDB_API_KEY: '',
+        FIREBASE_API_KEY: '',
+        FIREBASE_AUTH_DOMAIN: '',
+        FIREBASE_PROJECT_ID: '',
+        FIREBASE_STORAGE_BUCKET: '',
+        FIREBASE_MESSAGING_SENDER_ID: '',
+        FIREBASE_APP_ID: ''
     };
 };
 
